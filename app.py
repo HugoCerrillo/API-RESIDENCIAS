01b3ccb0eb225826862693ac5aa30f4bc8a8f228
HugoCerrillo <hugolcano85@gmail.com>
2026-03-30 09:14:46 -0600

    reparando login

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, jsonify
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask import Flask, request, jsonify, make_response
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, set_access_cookies
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import pymysql
@@ -10,7 +10,10 @@ from models import db, Usuario
 pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
+
+# 1. CORS: Permitimos cualquier origen temporalmente para pruebas
+CORS(app, supports_credentials=True, origins=["http://localhost:5173", "https://tu-proyecto.vercel.app"])
 
 #configuracion para la bd en AWS RDS
 DB_USER = "admin"
@@ -23,6 +26,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{DB_USER}:{DB_PASS}@{D
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=2) # <--- Aquí configuramos las 2 horas
 app.config['JWT_SECRET_KEY'] = 'qJTJ%_(7(t(FW2ggS5X8#h!1ftm!i+'
+
+app.config['JWT_TOKEN_LOCATION'] = ['cookies']
+app.config['JWT_COOKIE_SECURE'] = False  # Solo enviar por HTTPS (necesario en producción)
+app.config['JWT_COOKIE_CSRF_PROTECT'] = True # Protección contra ataques CSRF
+app.config['JWT_ACCESS_COOKIE_PATH'] = '/'
+app.config['JWT_COOKIE_SAMESITE'] = 'None' # Necesario si Vercel y AWS están en dominios distintos (LAX PARA LOCAL por ahora)
 jwt = JWTManager(app)
 db.init_app(app)
 
@@ -36,14 +45,17 @@ def login():
     user = Usuario.query.filter_by(correo=email).first()
 
     if user and user.contraseña == password:
-        access_token = create_access_token(identity=str(user.id_usuario))
-        return jsonify({
+        access_token = create_access_token(identity=str(user.id_usuario))        
+        response = make_response(jsonify({
             "status": "success",
-            "message": f"Sesión iniciada. Bienvenid@ {user.nombre}",
-            "token": access_token,
-            "expires_in_hours": 2,
-            "user": user.to_dict() # Usamos el método que creamos en el modelo
-        }), 200
+            "message": f"Bienvenid@ {user.nombre}",
+            "user": user.to_dict()
+        }))
+
+        # Seteamos la cookie en el navegador
+        set_access_cookies(response, access_token)
+        
+        return response, 200
     
     return jsonify({"status": "error", "message": "Correo o contraseña incorrectos"}), 401
 
