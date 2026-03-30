25bed6d48011ed0b793d5befa28962eb4cb2b41f
HugoCerrillo <hugolcano85@gmail.com>
2026-03-30 08:59:23 -0600

    corrigiendo login

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import pymysql
+from datetime import timedelta
 from models import db, Usuario
 
 #driver para conectar Python con MySQL
@@ -19,6 +21,9 @@ DB_NAME = "expertrack"
 #construcción de la URI de conexión
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=2) # <--- Aquí configuramos las 2 horas
+app.config['JWT_SECRET_KEY'] = 'qJTJ%_(7(t(FW2ggS5X8#h!1ftm!i+'
+jwt = JWTManager(app)
 db.init_app(app)
 
 @app.route('/login', methods=['POST'])
@@ -31,13 +36,16 @@ def login():
     user = Usuario.query.filter_by(correo=email).first()
 
     if user and user.contraseña == password:
+        access_token = create_access_token(identity=str(user.id_usuario))
         return jsonify({
             "status": "success",
-            "message": f"Bienvenido, {user.nombre}",
-            "user": user.to_dict()
+            "message": f"Sesión iniciada. Bienvenid@ {user.nombre}",
+            "token": access_token,
+            "expires_in_hours": 2,
+            "user": user.to_dict() # Usamos el método que creamos en el modelo
         }), 200
     
-    return jsonify({"status": "error", "message": "Credenciales inválidas"}), 401
+    return jsonify({"status": "error", "message": "Correo o contraseña incorrectos"}), 401
 
 #verificar conexiona la bd en aws
 @app.route('/check-connection-bd')
